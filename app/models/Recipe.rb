f9611f5c52d931c622d9e3f080ce440e65cb2ab8
Juan Antonio Sosa <juanan.sosa@gmail.com>
2016-12-31 00:10:28 +0100

    changed view

--- app/models/Recipe.rb
+++ app/models/Recipe.rb
@@ -1,17 +1,23 @@
 require 'win32ole'
+#require 'httparty'
+#require 'pp'
 
 class Recipe 
 	include HTTParty
 
-	#'882aeb66866bf7916d3af3fa1e295a28'
+	ENV['FOOD2FORK_KEY'] = '882aeb66866bf7916d3af3fa1e295a28'
 	key_value = ENV['FOOD2FORK_KEY']
-	hotsport = #ENV['FOOD2FORK_SERVER_AND_PORT']
+	hotsport = ENV['FOOD2FORK_SERVER_AND_PORT']
 
 	base_uri "http://#{hotsport}/api/"
 	default_params key: key_value
 	format :json
 
+	puts default_params
+
 	def self.for term
 		 get("/search", query: {q: term})["recipes"]
 	end
 end
+
+#pp Recipe.for('chocolate')
